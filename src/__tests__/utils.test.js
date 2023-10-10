1128156eb20be88690183f16e9ccbd491155ab67
Connor Adams <connorvadams@gmail.com>
2023-10-10 10:17:21 -0600

    Submits lab.

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,38 @@
-// Your tests here
+import { isPalindrome } from '../utils.js';
+
+describe('isPalindrome', () => {
+    it('returns false if a word is not a palindrome', () => {
+        const word = 'car'
+        const bool = isPalindrome(word)
+        expect(bool).toBe(false)
+    })
+
+    it('returns true if a word is not a palindrome', () => {
+        const word = 'racecar'
+        const bool = isPalindrome(word)
+        expect(bool).toBe(true)
+    })
+
+    it('handles upper and lower case letters', () => {
+        const word = 'rAcEcaR'
+        const bool = isPalindrome(word)
+        expect(bool).toBe(true)
+    })
+
+    it('returns false for an empty string', () => {
+        const bool = isPalindrome('')
+        expect(bool).toBe(false)
+    })
+
+    it('throws an error in the event of a special character', () => {
+        expect(() => {
+            isPalindrome('r@c3c4r')
+        }).toThrow()
+    })
+
+    it("throws an error when the input isn't a string", () => {
+        expect(() => {
+            isPalindrome(100)
+        }).toThrow()
+    })
+});
--- src/utils.js
+++ src/utils.js
@@ -1,1 +1,15 @@
-// Your code here
+const isPalindrome = (word) => {
+    if (word === '') {
+        return false
+    } else if (!/^[A-Za-z]+$/.test(word)) {
+        throw new Error('No special characters allowed')
+    } else if (typeof(word) !== 'string') {
+        throw new Error('Strings only')
+    } else {
+        const cleanedWord = word.replace(/\s/g, '').toLowerCase()
+        const reversedWord = cleanedWord.split('').reverse().join('')
+        return reversedWord === cleanedWord
+    }
+}
+
+export { isPalindrome }
